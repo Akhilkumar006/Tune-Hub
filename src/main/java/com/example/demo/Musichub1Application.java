6802060b7bd525d8f2d42f6792a7fa30fa93e8c9
akhil <akhilkumar.k006>
2024-02-21 22:21:02 +0530

    tunehub project

---
+++ src/main/java/com/example/demo/Musichub1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Musichub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Musichub1Application.class, args);
+	}
+
+}
